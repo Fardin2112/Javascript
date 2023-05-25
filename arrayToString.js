ae0020b4353d3a4c42219c16e9a55649ff2aff03
Fardin2112 <fardinalam2112@gmail.com>
2023-05-25 20:53:07 +0530

    update

--- arrayToString.js
+++ arrayToString.js
@@ -1,5 +1,10 @@
 let myname = ["Fardin"]
 console.log(typeof myname)
 
+// array to string
 myname = JSON.stringify(myname)
+console.log(typeof myname)
+
+// conversion again into string to Array
+myname = JSON.parse(myname)
 console.log(typeof myname)
