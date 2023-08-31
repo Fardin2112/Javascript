c03e2a185cc0c98858aa856c2d3a8b77a254cf67
Fardin2112 <fardinalam2112@gmail.com>
2023-08-31 11:45:02 +0530

    update

--- Reactjs.js
+++ Reactjs.js
@@ -6,3 +6,8 @@ const person = {
 // ... copy same as person
 const person2 = {...person,name:"chiku"}
 console.log(person2)
+
+const name = ["Fardin","chiku","Furious"];
+// copy same as name but add more names
+const name2 = [...name,"noku","Goku"];
+console.log(name2)
